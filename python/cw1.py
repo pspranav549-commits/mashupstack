3bb5064e44a98c4564244a131e2ed5891fbd319c
Pranav <pspranav549@gmail.com>
2026-02-11 12:52:52 +0530

    juice shop

---
+++ python/cw1.py
@@ -0,0 +1,18 @@
+apple = 15.5
+orange = 20
+grape= 10.25
+total_volume = apple + orange+ grape
+print("Total volume  sold is:", total_volume)
+
+total_int = int(total_volume)
+print("Total volume sold in integer is:", total_int)    
+
+total_str = str(total_volume)
+print("Total volume sold in string is:", total_str) 
+
+bonus = 9
+print("Bonus  volume is:", bonus)   
+
+final = total_volume + bonus
+print("Final volume  is:", final)   
+
